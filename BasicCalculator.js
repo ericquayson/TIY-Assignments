9b0467b2d30b996a1bdab83c8cc199a6f989a266
Eric <ecbaiden@ncsu.edu>
2015-09-03 02:38:44 -0400

    Addition, Subtraction, Dividing, Multiplying

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -1,11 +1,50 @@
-console.assert(add(0, 0), /* FILL ME IN */);
-console.assert(add(0, 1), /* FILL ME IN */);
-console.assert(add(0, 2), /* FILL ME IN */);
+function add(A, B){
+  return A + B;
+} // END add
+
+console.assert(add(0, 0) === 0); /* FILL ME IN */
+console.assert(add(0, 1) === 1); /* FILL ME IN */
+console.assert(add(0, 2) === 2);
+console.assert(add(0, 3) === 3);
+console.assert(add(0, 4) === 4);
+console.assert(add(0, 5) === 5);
+console.assert(add(0, 6) === 6);
+console.assert(add(0, 7) === 7);
+console.assert(add(0, 8) === 8);
+console.assert(add(0, 9) === 9);
+ /* FILL ME IN */
 
 /**
  * @param {Number} A to add to B
  * @param {Number} B to add to A
  * @return {Number} sum of A and B
  */
-function add(A, B){
-} // END add
+
+function multiply(A, B){
+  return A * B;
+} // End mulitple
+
+console.assert(multiply(0, 0) === 0);
+console.assert(multiply(0, 1) === 1);
+
+function subtract(A, B){
+  return A - B;
+} // End subtract
+
+console.assert(subtract(0, 0) === 0);
+console.assert(subtract(1, 0) === 1);
+console.assert(subtract(2, 0) === 2);
+console.assert(subtract(3, 0) === 3);
+console.assert(subtract(4, 0) === 4);
+console.assert(subtract(5, 0) === 5);
+console.assert(subtract(6, 0) === 6);
+console.assert(subtract(7, 0) === 7);
+console.assert(subtract(8, 0) === 8);
+console.assert(subtract(9, 0) === 9);
+
+function divide(A, B){
+  return A / B;
+} // End divide
+
+console.assert(divide(6, 3) === 2);
+console.assert(divide(49, 7) === 7 ;)
