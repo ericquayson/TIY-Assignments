ba61b7c82bb53bb8065ddfc66058bb0532a1a739
Eric <ecbaiden@ncsu.edu>
2015-09-08 21:12:59 -0400

    added mocha and chai

--- APIGymnastics/Etsy.js
+++ APIGymnastics/Etsy.js
@@ -2,9 +2,12 @@
 var items = require('items.json');
 
 // TODO: Setup mocha and chai...
+var test = require('mocha').it,
+  expect = require('chai').expect;
 
 test('this is the easy one', function(){
   expect(Array.isArray(items)).to.be.true; // What.
+
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
 
   expect(items.length).to.equal(FILL_ME_IN); // It's haunting me now...
