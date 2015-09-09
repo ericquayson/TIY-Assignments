bb5ba6f4392d9de8ad65dffea9c8efb16be596de
Eric <ecbaiden@ncsu.edu>
2015-09-09 11:20:43 -0400

    refractoring from long to short code

--- APIGymnastics/Etsy.js
+++ APIGymnastics/Etsy.js
@@ -6,7 +6,7 @@ var test = require('mocha').it,
   expect = require('chai').expect;
 
 test('this is the easy one', function(){
-  expect(Array).to.exist;
+  expect(items).to.exist;
   expect(Array.isArray(items)).to.be.true; // What.
 
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
@@ -15,7 +15,75 @@ test('this is the easy one', function(){
 });
 
 test('finding the average price', function(){
-  var yourAnswer = "start with `items`; use `Array` methods";
+  // var yourAnswer = "start with `items`; use `Array` methods";
+  var yourAnswer = 0;
+
+  // To calculate the average price, we need:
+  // * each item's price
+  var prices = [ ], item;
+
+  function forEachItem(index, length){
+    // For the first item in the list...
+    item = items[index];
+
+    expect(item).to.be.an('object');
+    expect(item.price).to.be.a('number');
+    // expect(item.price).to.be.equal(12);
+
+    prices.push(item.price);
+
+    // expect(prices).to.deep.equal([ 12 ]);
+    expect(prices.length).to.equal(length);
+  }
+
+  var index, length;
+
+  // for (var index = 0; index < items.length; index++) {
+  //   item = items[index];
+  //   prices.push(item.price);
+  // }
+
+  items.forEach(function(item, index, all){
+    forEachItem(index, index + 1);
+  })
+  // forEachItem(0, 1);
+  // forEachItem(1, 2);
+  // forEachItem(2, 3);
+  // forEachItem(3, 4);
+  // index = 0; length = 1;
+  // {
+  //   // For the first item in the list...
+  //   item = items[index];
+  //
+  //   expect(item).to.be.an('object');
+  //   expect(item.price).to.be.a('number');
+  //   // expect(item.price).to.be.equal(12);
+  //
+  //   prices.push(item.price);
+  //
+  //   // expect(prices).to.deep.equal([ 12 ]);
+  //   expect(prices.length).to.equal(length);
+  // }
+
+  // index = 1; length = 2;
+  // {
+  //   // For the second item in items...
+  //   item = items[index];
+  //
+  //   expect(item).to.be.an('object');
+  //   expect(item.price).to.be.a('number');
+  //   // expect(item.price).to.be.equal(20);
+  //
+  //   prices.push(item.price);
+  //
+  //   // expect(prices).to.deep.equal([ 12, 20 ])
+  //   expect(prices.length).to.equal(length);
+  // }
+  // * sum of all item's prices
+  // * total number of items
+  //
+  // sum of all item prices / number of items
+  expect(items.length).to.equal(25);
 
   expect(yourAnswer).to.be.closeTo(23.63, 0.01);
 });
@@ -25,14 +93,7 @@ test('finding that perfect $15 item', function(){
    * @param {Array} items to search through
    * @return {Array} of `items` with `price` between `min` and `max` USD
    */
-  function pricedBetween(items, min, max){
-    // Just a suggestion, really...
-      var items15 = [];
-      for (var i = 0; i < 23.63 && i > .01; i++) {
-        items15.push[i]
-      }
-      return items15;
-  }
+
 
   expect( pricedBetween(items, 14, 18) ).to.deep.equal([
     // Can you find their names _without_ code first?
