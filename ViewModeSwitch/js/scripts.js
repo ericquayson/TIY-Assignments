2b0e4669fe824404dfd607a33df10a260685345f
Eric <ecbaiden@ncsu.edu>
2015-09-10 08:46:46 -0400

    comments and DOM

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -1,1 +1,6 @@
-
+//JavaScript is allowing for the user to render two differnt viewing styles on the same page.
+//This does not involve any routing magic
+//Bascially once the user has clicked on the non-highlighted box, the page body of the page will render a new
+//layout style
+//
+//The DOM elements needed for ViewModeSwitch is the body element, p, header tags
