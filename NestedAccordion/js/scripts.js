557bacdfa3c09c5f33d5aad41d49733c6f435d9f
Eric <ecbaiden@ncsu.edu>
2015-09-10 11:12:46 -0400

    new comments from HW review

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -1,3 +1,9 @@
-//The JavaScript is making the NestedAccordin hide and show elements
+/**
+ * ## What does it do?
+ *
+ * The JavaScript is making the NestedAccordin hide and show elements
 
-//The DOM elements that will be needed are UL, LI and P tags
+ * The DOM elements that will be needed are UL, LI and P tags
+ * The `<li>` that contains the heading I click turns into `<li class='cbp-ntopen">`
+ *   - When I click ka kheading with text visible (open accordion);
+ *   - the `<li class="cbp-ntopen">` that contains the headig I click turns into `<li>`
