e486441f9a6fd761443c79e008be135caf869eeb
Eric <ecbaiden@ncsu.edu>
2015-09-13 14:47:31 -0400

    stuff

--- NestedAccordion/js/scripts.js
+++ NestedAccordion/js/scripts.js
@@ -5,7 +5,7 @@
  * The DOM elements that will be needed are UL, LI and P tags
  * The `<li>` that contains the heading I click turns into `<li class='cbp-ntopen">`
  *   - When I click the heading with text visible (open accordion);
- *   - the `<li class="cbp-ntopen">` that contains the headig I click turns into `<li>`
+ *   - the `<li class="cbp-ntopen">` that contains the heading I click turns into `<li>`
 */
 //need to add click events (addEventListener)
 
@@ -13,7 +13,7 @@ var heading = document.querySelector('.cbp-nttrigger');
 
  $(function () {
    $('.cbp-nttrigger').click(function () {
-     $('.cbp-ntcontent').show();
+     $('.cbp-ntopen').show();
    });
  });
 
@@ -32,6 +32,7 @@ var heading = document.querySelector('.cbp-nttrigger');
  //heading.addEventListener('click', tracer);
  // two differnt options of defining and using the function once the user has
  // clicked on the header
+
 
  //heading.addEventListener('click', function () {
    // The text below hides and shows.
