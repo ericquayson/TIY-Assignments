22e445214a3f181c34779e022f08c5209fc48d9f
Eric <ecbaiden@ncsu.edu>
2015-09-13 16:21:15 -0400

    Updated files

---
+++ ViewModeSwitch/js/scripts.js
@@ -0,0 +1,14 @@
+/**
+/ Once the user see's the unhlighted switch button at the top of the page +
+/ , the user can decide whether or not to click the button. This unhlighted button +
+/ when clicked will render the same content but in a different lay on the page
+/ Additionally, the highlighted button will be selected in the cbpViewModeSwitch
+/
+/ `cbp-vm-selected`
+*/
+
+
+
+$(function () {
+  $()
+});
