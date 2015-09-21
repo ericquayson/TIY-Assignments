b208552d2a1acfe41bf81bed9007d318800d2979
Eric <ecbaiden@ncsu.edu>
2015-09-20 20:46:05 -0400

    updating responsive

--- TodoMVC/js/app.js
+++ TodoMVC/js/app.js
@@ -1,6 +1,30 @@
+/* 
+===========================Pseudo Code============================
+
+Create a function that is going to add and append the to do
+	to do element under the parent element after the user has clicked on the button
+	to add the to do list item
+
+	I have to make sure that I take screenshots of this happening
+	in the console on the browser. Upload the image into my file,
+
+	Using jQuery will make things easier
+			* add jQuery CDN to the HTML page
+
+	Task
+			* capture the `<input>` from the browswer
+			* print it to the console
+			* take a screenshot and include it in the issue
+			* I need an `addEventListener`
+
+*/
+
 (function (window) {
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+	function function_name(argument) {
+		// body...
+	}
 
 })(window);
