3c28fa7114321c0ba0d6a19281c51345be1db827
ericquayson <ericquayson@github.com>
2015-10-11 13:44:51 -0400

    :hover is selecting the first row... not the target, but it is working

--- Halftime/McGriddles/src/js/main.js
+++ Halftime/McGriddles/src/js/main.js
@@ -1,5 +1,14 @@
+/* tracer bullet */
 $(document).ready(function() {
-  $('img').on('click', function() {
+  $('img').hover(function() {
    console.log('im working');
   });
 });
+
+/* Once a user hovers over the image, the h2 is supposed to toggle
+$(document).ready(function() {
+  $('img').hover(function () {
+    $('h2').toggle();
+  });
+});
+*/
